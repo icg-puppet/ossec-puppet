5c8415b5ae544b8e6548b30021e111a2673581ab
Brett Miller <brett.miller@ingramcontent.com>
2016-08-19 16:40:25 -0500

    add upstream changes to addlog.pp into addlog_command.pp

--- manifests/addlog_command.pp
+++ manifests/addlog_command.pp
@@ -3,15 +3,23 @@ define ossec::addlog_command(
   $logtype = 'command',
   $logcommand,
   $commandalias,
+  $agent_log_cmd = true,
   $logfrequency = '60',
 ) {
   require ossec::params
+# Issue #30
+  if $agent_log_cmd
+  {
+    $ossec_notify_cmd = Service[$ossec::params::agent_service]
+  } else {
+    $ossec_notify_cmd = Service[$ossec::params::server_service]
+  }
 
   concat::fragment { "ossec.conf_21-${commandalias}":
     target  => $ossec::params::config_file,
     content => template('ossec/21_ossecLogfile_command.conf.erb'),
     order   => 21,
-    notify  => Service[$ossec::params::server_service]
+    notify  => $ossec_notify_cmd
   }
 
 }
